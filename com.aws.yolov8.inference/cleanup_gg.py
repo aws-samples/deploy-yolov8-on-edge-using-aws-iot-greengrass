ac778346e139942decd6c3bd1e9d80276f813311
Romil Shah <rpshah@amazon.com>
2023-02-26 19:20:44 -0800

    Update cleanup_gg.py

--- com.aws.yolov8.inference/cleanup_gg.py
+++ com.aws.yolov8.inference/cleanup_gg.py
@@ -1,7 +1,7 @@
-AWS_ACCOUNT_NUM="ADD_ACCOUNT_NUMBER"
-AWS_REGION="ADD_REGION"
-DEV_IOT_THING="NAME_OF_OF_THING"
-DEV_IOT_THING_GROUP="NAME_OF_IOT_THING_GROUP"
+AWS_ACCOUNT_NUM="348657513258"
+AWS_REGION="us-east-1"
+DEV_IOT_THING="NvidiaOrinGreengrassCore"
+DEV_IOT_THING_GROUP="NvidiaOrinGreengrassCoreGroup"
 
 DEV_IOT_COMPONENT="com.aws.yolov8.inference"
 
@@ -10,14 +10,16 @@ import boto3
 gg_client = boto3.client('greengrassv2')
 
 # Delete all the AWS IoT Greengrass Components
+component_arn = None
 response = gg_client.list_components()
 for component in response['components']:
     if component['componentName']==DEV_IOT_COMPONENT:
         component_arn = component['arn']
-response = gg_client.list_component_versions(arn=component_arn)
-for component_ver in response['componentVersions']:
-    gg_client.delete_component(arn=component_ver['arn'])
-    print(f"Deleted component: {component_ver['arn']}")
+if component_arn!=None:
+    response = gg_client.list_component_versions(arn=component_arn)
+    for component_ver in response['componentVersions']:
+        gg_client.delete_component(arn=component_ver['arn'])
+        print(f"Deleted component: {component_ver['arn']}")
 
 # Delete all the AWS IoT Greengrass Deployments
 deployments = gg_client.list_deployments(targetArn=f"arn:aws:iot:{AWS_REGION}:{AWS_ACCOUNT_NUM}:thing/{DEV_IOT_THING}", historyFilter='ALL')['deployments']
@@ -29,4 +31,3 @@ for deployment in deployments:
             gg_client.cancel_deployment(deploymentId=deployment_id)
         gg_client.delete_deployment(deploymentId=deployment_id)
         print(f"Deleted deployment: {deployment_arn} : {deployment_id} : {deployment['revisionId']}")
-
